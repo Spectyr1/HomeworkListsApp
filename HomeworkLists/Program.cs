aa229fb5b306dab085eda629bcbada0c028d4679
Danny Warren <danny@theforbiddenarts.com>
2022-03-24 22:20:31 -0400

    submit

--- HomeworkLists/Program.cs
+++ HomeworkLists/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+List<string> students = new List<string>();
+bool done = false;
+
+do
+{
+    Console.Write("Enter the students name to add to to roster. enter a 'BLANK' name to complete: ");
+    string? name = Console.ReadLine();
+
+    if (name != "")
+    {
+        students.Add(name);
+    }
+    else
+    {
+        done = true;
+    }
+} while (done != true);
+
+Console.WriteLine($"You have registered {students.Count()} into the class.");
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 HomeworkListsApp
+
+Add students to a class roster until there are no more students.
+Then print out the counter of the students to the Console.
